054c8688d98a5215d50425c68c6d9828a5b81615
Tyler Burnett <11090043+TylerBurnett@users.noreply.github.com>
2024-08-18 14:01:57 +1000

    feat: added breakpoint based resizing for resume preview

--- src/features/home/ResumePreview.tsx
+++ src/features/home/ResumePreview.tsx
@@ -1,31 +1,52 @@
-import React from "react";
 import { Document, Page } from "react-pdf";
 import Resume from "../../assets/pdf/Resume.pdf?url";
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, useTheme } from "@mui/material";
 import { pdfjs } from "react-pdf";
+import React, { useEffect } from "react";
+import { CalculateCanvasSize } from "../../helpers/dynamicCanvasScaler";
+import { useWindowWidth } from "@react-hook/window-size";
 
 // This is needed for pdfjs to function
 pdfjs.GlobalWorkerOptions.workerSrc = new URL("pdfjs-dist/build/pdf.worker.min.mjs", import.meta.url).toString();
 
-export default function ResumePreview() {
-	const options = {
-		cMapUrl: "cmaps/",
-		cMapPacked: true,
-		standardFontDataUrl: "standard_fonts/",
-	};
+const options = {
+	cMapUrl: "cmaps/",
+	cMapPacked: true,
+	standardFontDataUrl: "standard_fonts/",
+};
 
-	const downloadFile = (fileUrl: string, fileName: string) => {
-		const a = document.createElement("a");
-		a.href = fileUrl;
-		a.setAttribute("download", fileName);
-		a.click();
-	};
+const downloadFile = (fileUrl: string, fileName: string) => {
+	const a = document.createElement("a");
+	a.href = fileUrl;
+	a.setAttribute("download", fileName);
+	a.click();
+};
+
+export default function ResumePreview() {
+	// We are relying on the useEffect inside this hook for re-rendering the component
+	const windowWidth = useWindowWidth();
+
+	const theme = useTheme();
+	const canvasSize = CalculateCanvasSize(theme, {
+		xs: {
+			width: "80vw",
+			height: "",
+		},
+		md: {
+			width: "50vw",
+			height: "",
+		},
+		lg: {
+			width: "30vw",
+			height: "",
+		},
+	});
 
 	return (
-		<Grid container direction="column" alignContent="center">
+		<Grid container alignContent="center" alignItems="center" direction="column">
 			<Grid item>
 				<Document file={Resume} options={options}>
-					<Page pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} />
+					<Page width={canvasSize.width} pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} />
 				</Document>
 			</Grid>
 			<Grid item>
---
+++ src/helpers/dynamicCanvasScaler.ts
@@ -0,0 +1,38 @@
+import type { Theme } from "@mui/material";
+import { pixels } from "@pacote/pixels";
+
+export type ResponsiveBreakpoint = {
+	height: string;
+	width: string;
+};
+
+export type CanvasSize = {
+	height: number;
+	width: number;
+};
+
+export const CalculateCanvasSize = (
+	theme: Theme,
+	breakPoints: { xs?: ResponsiveBreakpoint; sm?: ResponsiveBreakpoint; md?: ResponsiveBreakpoint; lg?: ResponsiveBreakpoint; xl?: ResponsiveBreakpoint },
+): CanvasSize => {
+	console.log(theme.breakpoints.values);
+
+	const breakpoints = Object.entries(theme.breakpoints.values)
+		.map((b) => b[1])
+		.filter((b) => b < window.innerWidth);
+
+	console.log(breakpoints);
+
+	const closestBreakpoint = [breakPoints.xs, breakPoints.sm, breakPoints.md, breakPoints.lg, breakPoints.xl]
+		.slice(0, breakpoints.length)
+		.filter((v) => v !== undefined)
+		.at(-1);
+
+	if (closestBreakpoint !== undefined)
+		return {
+			height: pixels(closestBreakpoint.height),
+			width: pixels(closestBreakpoint.width),
+		};
+
+	return { height: 0, width: 0 };
+};
