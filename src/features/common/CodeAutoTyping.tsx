b6b74c90364a456b8387c315caa9ca1e871d423d
Tyler Burnett <11090043+TylerBurnett@users.noreply.github.com>
2024-08-22 19:14:40 +1000

    feat: added intersection observer to code typing component

--- src/features/common/CodeAutoTyping.tsx
+++ src/features/common/CodeAutoTyping.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import Typist from "react-typist-component";
 import { PrismAsyncLight as SyntaxHighlighter, type SyntaxHighlighterProps } from "react-syntax-highlighter";
+import Typist from "react-typist-component";
 import "./CodeAutoTyping.css";
+import { useInView } from "react-intersection-observer";
 
 export interface CodeAutoTypingProps {
 	text: string | string[];
@@ -12,9 +13,11 @@ export interface CodeAutoTypingProps {
 function CodeAutoTyping(props: CodeAutoTypingProps) {
 	const { text, language, syntaxHighlighterProps } = props;
 
+	const { ref, inView } = useInView();
+	
 	return (
-		<div className="code-snippet" style={{ minWidth: "100%", minHeight: "300px" }}>
-			<Typist typingDelay={100} cursor={<span className="cursor">|</span>}>
+		<div className="code-snippet" style={{ minWidth: "100%", minHeight: "300px" }} ref={ref}>
+			<Typist typingDelay={100} cursor={<span className="cursor">|</span>} pause={!inView}>
 				<SyntaxHighlighter showLineNumbers wrapLines language={language} {...syntaxHighlighterProps}>
 					{text}
 				</SyntaxHighlighter>
